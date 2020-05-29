39ed130e9e4f66c1593814935314dff2b3668541
LeeRobb198 <1601432@rgu.ac.uk>
2020-05-29 15:55:55 +0100

    Adding angular

---
+++ room-app/server/database/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+    db: 'mongodb+srv://LeeRobb198:muizD6QhKSn5uYKn@cluster0-fagn0.mongodb.net/game?retryWrites=true&w=majority'
+   };
---
+++ room-app/server/models/room.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+// Define collection and schema -----------------------------------------------
+
+const RoomSchema = mongoose.Schema({
+    name: { type: String },
+    up: { type: String },
+    down: { type: String },
+    left: { type: String },
+    right: { type: String },
+    items: { type: Array }
+});
+
+module.exports = mongoose.model('Room', RoomSchema);
